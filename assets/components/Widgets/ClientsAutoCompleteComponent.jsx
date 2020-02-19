43468fbcc800ee490f6bfeb8a110d57dd9ea4a23
cdelouvencourt <c.delouvencourt@nocturne.app>
2020-02-19 14:13:55 +0100

    add autocomplete

--- assets/components/Widgets/ClientsAutoCompleteComponent.jsx
+++ assets/components/Widgets/ClientsAutoCompleteComponent.jsx
@@ -1,40 +1,67 @@
 import React from "react";
-import Autosuggest from 'react-autosuggest';
+import Autocomplete from 'react-autocomplete';
+import Avatar from "react-avatar";
 
 export default class ClientsAutoCompleteComponent extends React.Component {
 
-  constructor(props) {
+  state = {
+    value: "",
+    clients: []
+  };
+
+  constructor(props){
     super(props);
+    this.getSuggestions = this.getSuggestions.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({clients: this.props.clients})
   }
 
   getSuggestions(value) {
     const inputValue = value.trim().toLowerCase();
     const inputLength = inputValue.length;
+    this.setState({value});
 
-    return inputLength === 0 ? [] : this.props.clients.filter(lang =>
-      lang.name.toLowerCase().slice(0, inputLength) === inputValue
-    );
-  }
-
-  getSuggestionValue(suggestion) {
-    return suggestion.entrepriseName ? suggestion.entrepriseName : suggestion.fullName;
-  }
-
-  renderSuggestion(suggestion) {
-    return (
-      <div>
-        {suggestion.name}
-      </div>
-    );
+    this.setState({clients: inputLength === 0 ? [] : this.state.clients.filter(client =>
+        client.entrepriseName.trim().toLowerCase().slice(0, inputLength) === inputValue || client.fullName.trim().toLowerCase().slice(0, inputLength) === inputValue
+      )});
   }
 
   render(){
     return (
-      <Autosuggest
-        suggestions={this.props.clients}
-        getSuggestionValue={this.getSuggestionValue}
-        renderSuggestion={this.renderSuggestion}
+      <Autocomplete
+        getItemValue={(item) => item.entrepriseName ? item.entrepriseName : item.fullName}
+        items={this.state.clients}
+        renderInput={(props) => (
+          <div className="dropdown-trigger">
+            <div className="field is-fullwidth">
+              <p className="control is-expanded has-icons-right is-fullwidth">
+                <input className="input input_form_custom is-fullwidth" type="text" placeholder={"Rechercher un client..."} {...props}/>
+                <span className="icon is-small is-right"><i className="fas fa-search"></i></span>
+              </p>
+            </div>
+          </div>
+        )}
+        renderMenu={(items, value, style) => (
+          <div className="dropdown is-active">
+            <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                <div className="dropdown-content" children={items}>
+                </div>
+            </div>
+          </div>
+        )}
+        renderItem={(item, isHighlighted) =>
+          <div className="dropdown-item" style={{ background: isHighlighted ? 'lightgray' : 'white', flexDirection: 'row' }}>
+            <Avatar name={item.entrepriseName ? item.entrepriseName : item.fullName} round="50px" size={30} style={{marginRight: 10}} />
+            {item.entrepriseName ? item.entrepriseName : item.fullName}
+          </div>
+        }
+        value={this.state.value}
+        onChange={(e) => this.getSuggestions(e.target.value)}
+        onSelect={(val) => this.setState({value: val})}
       />
+
     );
   }
 
